ea46fc7e059f1b1ebedc2f838607119dcf50ba0a
Hank Xie <hank_xie@varroctyc.com>
2018-07-11 15:58:18 +0800

    Add bat files

---
+++ create_hotspot.bat
@@ -0,0 +1,3 @@
+@echo off
+
+netsh wlan set hostednetwork mode=allow ssid=%computername% key=12345678
---
+++ delete_hotspot.bat
@@ -0,0 +1,3 @@
+@echo off
+
+netsh wlan set hostednetwork mode=disallow ssid=%computername% key=12345678
